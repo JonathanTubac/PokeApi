3d9ec24e5ffa47b87fc72361f2cb2cb1dd5d58e4
JonathanTubac <javiertubac1290.e@gmail.com>
2025-09-25 00:25:10 -0600

    feat(ViewModel): ViewModel for pokemon list implemented

--- app/src/main/java/com/uvg/pokeapi/ui/viewmodel/PokemonViewModel.kt
+++ app/src/main/java/com/uvg/pokeapi/ui/viewmodel/PokemonViewModel.kt
@@ -1,2 +1,15 @@
 package com.uvg.pokeapi.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.uvg.pokeapi.data.repository.PokemonRepository
+import kotlinx.coroutines.Dispatchers
+
+class PokemonViewModel : ViewModel() {
+    private val repository = PokemonRepository()
+
+    val pokemonList = liveData(Dispatchers.IO) {
+        val response = repository.getPokemonList()
+        emit(response.body()?.results ?: emptyList())
+    }
+}
